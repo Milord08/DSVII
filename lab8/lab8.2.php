66bf2527796b44de69f1ecc65f665586f59edd7e
Milord08 <ricardo.milord@utp.ac.pa>
2023-11-02 15:59:02 -0500

    lab8

---
+++ lab8/lab8.2.php
@@ -0,0 +1,72 @@
+<?php
+
+class FactorialCalculator{
+    private $numero;
+    private $factorial;
+
+    public function __construct() {
+        $this->numero = 0;
+        $this->factorial = 1;
+    }
+
+    public function setNumero($numero) {
+        $this->numero = intval($numero);
+    }
+
+    public function calcularFactorial() {
+        if ($this->numero < 0) {
+            return "El número debe ser positivo.";
+        }
+
+        for ($i = 2; $i <= $this->numero; $i++) {
+            $this->factorial *= $i;
+        }
+
+        return "El factorial de $this->numero es $this->factorial.";
+    }
+}
+
+class CalculatorV{
+    public function mostrarFormulario() {
+        echo '<h1>Calculadora Factorial</h1>';
+        echo '<form method="POST">';
+        echo '<label for="numero">Ingrese un número deseado:</label>';
+        echo '<input type="text" name="numero" id="numero" required>';
+        echo '<input type="submit" value="Calcular Factorial">';
+        echo '</form>';
+    }
+
+    public function mostrarResultado($resultado) {
+        echo '<h1>Resultado Factorial</h1>';
+        echo "<p>$resultado</p>";
+        echo '<p><a href="lab82.php">Volver al formulario</a></p>';
+    }
+}
+
+class FactorialCalculatorController {
+    private $model;
+    private $view;
+
+    public function __construct($model, $view) {
+        $this->model = $model;
+        $this->view = $view;
+    }
+
+    public function procesar() {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $this->model->setNumero($_POST["numero"]);
+            $resultado = $this->model->calcularFactorial();
+            $this->view->mostrarResultado($resultado);
+        } else {
+            $this->view->mostrarFormulario();
+        }
+    }
+}
+
+$model = new FactorialCalculator();
+$view = new CalculatorV();
+$controller = new FactorialCalculatorController($model, $view);
+
+$controller->procesar();
+
+?>
---
+++ lab8/lab8.3.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laboratorio 8.3</title>
+</head>
+<body>
+    <h1>Generador de Matriz</h1>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+        <label for="n">Ingrese el valor de N ( que sea un número par): </label>
+        <input type="number" name="n" required>
+        <button type="submit">Generar Matriz</button>
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $n = $_POST['n'];
+        $matrizObj = new MatrizIdentidad($n);
+        $matrizObj->mostrarMatriz();
+    }
+    ?>
+
+</body>
+</html>
+
+<?php
+class Matriz
+{
+    private $n;
+    private $matriz;
+
+    public function __construct($n)
+    {
+        $this->n = $n;
+        $this->generarMatriz();
+    }
+
+    private function generarMatriz()
+    {
+        $this->matriz = array();
+        for ($i = 0; $i < $this->n; $i++) {
+            $fila = array();
+            for ($j = 0; $j < $this->n; $j++) {
+                if ($i == $j) {
+                    $fila[] = 1;
+                } else {
+                    $fila[] = 0;
+                }
+            }
+            $this->matriz[] = $fila;
+        }
+    }
+
+    public function mostrarMatriz()
+    {
+        echo "<h2>Matriz Identidad de Orden $this->n:</h2>";
+        echo "<table border='1'>";
+        foreach ($this->matriz as $fila) {
+            echo "<tr>";
+            foreach ($fila as $elemento) {
+                echo "<td>$elemento</td>";
+            }
+            echo "</tr>";
+        }
+        echo "</table>";
+    }
+}
+
+?>
